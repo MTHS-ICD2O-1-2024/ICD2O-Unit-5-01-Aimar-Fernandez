c38e2a547b2528ab2e29ba2a51a7448df4d34065
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-16 17:29:27 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -9,5 +9,13 @@
 const randomNumber = Math.floor(Math.random() * 6) + 1
 
 function myButtonClicked() {
-  
+  const userNumber = parseInt(document.getElementById("user-number").value)
+  if (userNumber == randomNumber) {
+    document.getElementById("answer").innerHTML = 
+      "<p>You have guessed the correct number!</p>"
+  }
+  if (userNumber != randomNumber) {
+    document.getElementById("answer").innerHTML = 
+      "<p>You guessed incorrectly"
+  }
 }
