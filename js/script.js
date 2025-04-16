8fced32c93a03312ec902022343f42a5b26b4e4d
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-16 17:00:21 +0000

    s

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function myButtonClicked() {
+  
+}
